df41ca92d0232948768130cf712bbd7d4033d27f
px <1508115864@qq.com>
2024-01-16 07:08:26 -0500

    Site updated: 2024-01-16 07:07:44

---
+++ js/Q&A.js
@@ -0,0 +1,22 @@
+$("div[name='QA']").on('keydown', async (event) => {
+    if (event.isComposing || event.keyCode === 13) {
+  		QA(event.currentTarget);
+      }
+     }
+);
+
+function QA(e){
+    const Q = $($(e).find("span")[0]).text();
+    const answer = $(e).attr("answer").split(",");
+    console.log(answer);
+    console.log($($(e).find("input")[0]).val());
+    if($.inArray($($(e).find("input")[0]).val(),answer) >=0 ){
+    	$(e).css("display","none");
+    	$("div#"+Q).css("display","block");
+    	console.log("匹配");
+    }else{
+    	alert($(e).attr("data-wpm"));
+    	console.log("不匹配");
+    }
+
+}
