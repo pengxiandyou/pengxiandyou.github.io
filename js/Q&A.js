fa7abc83d973ffa7e1ed1fae1adfc6eb30604561
px <1508115864@qq.com>
2024-01-16 07:11:56 -0500

    Site updated: 2024-01-16 07:11:23

--- js/Q&A.js
+++ js/Q&A.js
@@ -8,15 +8,15 @@ $("div[name='QA']").on('keydown', async (event) => {
 function QA(e){
     const Q = $($(e).find("span")[0]).text();
     const answer = $(e).attr("answer").split(",");
-    console.log(answer);
-    console.log($($(e).find("input")[0]).val());
+    // console.log(answer);
+    // console.log($($(e).find("input")[0]).val());
     if($.inArray($($(e).find("input")[0]).val(),answer) >=0 ){
     	$(e).css("display","none");
     	$("div#"+Q).css("display","block");
-    	console.log("匹配");
+    	// console.log("匹配");
     }else{
     	alert($(e).attr("data-wpm"));
-    	console.log("不匹配");
+    	// console.log("不匹配");
     }
 
 }
